4768dba42a099f56cc0b5d6ce26139c229174bab
caalzate <80935443+caalzate@users.noreply.github.com>
2023-10-31 10:16:30 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    print(f"¡Hola, {nombre} desde GitHub actions!")
+    print(f"¡Hola, {nombre} desde GitHub actions 2da ejecucion!")
 
 if __name__ == "__main__":
     main()
